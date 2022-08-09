b2e326d769298f885731ebe800c4e631aa1245bb
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-08-09 19:43:53 +0900

    Update 4 Files
    필요 없는 파일 삭제 1
    코드 수정중 5~6
    README.md 필요 없는거 삭제

---
+++ preliminary_round/5.py
@@ -0,0 +1,25 @@
+
+N, M = map(int, input().split())
+
+canvas = []
+color_count = 0
+
+''' # Debug
+print(canvas)
+'''
+
+for i in range(M):
+    templist = list(map(int, input().split()))
+    canvas.append(templist)
+
+for i in range(1, 8):
+    if any(i in l for l in canvas):
+        color_count += 1
+    else:
+        pass
+
+''' # Debug
+print(canvas)
+'''
+print(color_count)
+print(f"H")
