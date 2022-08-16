6d0a98fb29ac6d18cbbcb93ff65191d351782f66
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-08-16 19:46:35 +0900

    Update 3 Files
    what

--- preliminary_round/8.py
+++ preliminary_round/8.py
@@ -1,10 +1,10 @@
-N = int(input())
-res = 0
-visit = 0
+N = int(input()) # n을 입력받습니다.
+res = 0 # res를 선언하고 0을 저장합니다.
+visit = 0 # visit을 선언하고 0을 저장합니다.
 
-memo = [[]for _ in range(N)]
+memo = [[]for _ in range(N)] # memo에 다차원 배열을 생성합니다
 
-for i, j in enumerate(list(map(lambda x: int(x)-1, input().split()))):
+for i, j in enumerate(list(map(lambda x: int(x)-1, input().split()))): 
     memo[j].append(i)
 
 for i in range(N):
